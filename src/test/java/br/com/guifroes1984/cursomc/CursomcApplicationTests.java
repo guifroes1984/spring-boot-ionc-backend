912939755cc538ffb3417a3080fa85fb5df52860
guifroes1984 <guilhermefroes26@gmail.com>
2023-08-24 17:12:16 -0300

    Projeto criado

---
+++ src/test/java/br/com/guifroes1984/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.guifroes1984.cursomc;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
