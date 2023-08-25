9c6c1e41d38faa0d44bb965c3070375d4f1113e6
guifroes1984 <guilhermefroes26@gmail.com>
2023-08-25 16:12:44 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/br/com/guifroes1984/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/guifroes1984/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.guifroes1984.cursomc.domain.Categoria;
 import br.com.guifroes1984.cursomc.repositories.CategoriaRepository;
+import br.com.guifroes1984.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,10 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Categoria.class.getName());
+		}
 		return obj;
 	}
 
