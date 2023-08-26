6e50ec5f3a4f585fea19ddeb9911e803a94cf323
guifroes1984 <guilhermefroes26@gmail.com>
2023-08-25 22:05:13 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/br/com/guifroes1984/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package br.com.guifroes1984.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.guifroes1984.cursomc.domain.Cliente;
+import br.com.guifroes1984.cursomc.repositories.ClienteRepository;
+import br.com.guifroes1984.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Cliente não encontrado! Id: " + id
+					+ ", Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+	}
+
+}
