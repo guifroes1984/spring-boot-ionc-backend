6b6ce45905d251405962ae94f9e376da8205cf59
guifroes1984 <guilhermefroes26@gmail.com>
2023-08-28 15:37:07 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/br/com/guifroes1984/cursomc/services/PedidoService.java
+++ src/main/java/br/com/guifroes1984/cursomc/services/PedidoService.java
@@ -3,21 +3,21 @@ package br.com.guifroes1984.cursomc.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.com.guifroes1984.cursomc.domain.Cliente;
-import br.com.guifroes1984.cursomc.repositories.ClienteRepository;
+import br.com.guifroes1984.cursomc.domain.Pedido;
+import br.com.guifroes1984.cursomc.repositories.PedidoRepository;
 import br.com.guifroes1984.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
-public class ClienteService {
+public class PedidoService {
 	
 	@Autowired
-	private ClienteRepository repo;
+	private PedidoRepository repo;
 	
-	public Cliente buscar(Integer id) {
-		Cliente obj = repo.findOne(id);
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
 		if (obj == null) {
 			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
-					+ ", Tipo: " + Cliente.class.getName());
+					+ ", Tipo: " + Pedido.class.getName());
 		}
 		return obj;
 	}
