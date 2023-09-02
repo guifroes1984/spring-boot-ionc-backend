1f29a98f01e6830d741ddfcf91dd948d4ed38f03
guifroes1984 <guilhermefroes26@gmail.com>
2023-09-02 20:41:37 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/br/com/guifroes1984/cursomc/config/DevConfig.java
+++ src/main/java/br/com/guifroes1984/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import br.com.guifroes1984.cursomc.services.DBService;
+import br.com.guifroes1984.cursomc.services.EmailService;
+import br.com.guifroes1984.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,5 +31,10 @@ public class DevConfig {
 		
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
