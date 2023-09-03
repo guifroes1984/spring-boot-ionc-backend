3af4754b830df96500aaf6d860fc49e9791e557f
guifroes1984 <guilhermefroes26@gmail.com>
2023-09-02 23:18:03 -0300

    Email HTML

--- src/main/java/br/com/guifroes1984/cursomc/services/EmailService.java
+++ src/main/java/br/com/guifroes1984/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package br.com.guifroes1984.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import br.com.guifroes1984.cursomc.domain.Pedido;
@@ -9,4 +11,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
